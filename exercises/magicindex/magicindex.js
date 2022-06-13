3f50b2e097568f4c6742150943ace77dc979d461
manila <3621543+manila@users.noreply.github.com>
2022-06-12 23:48:19 -0700

    folder delete

--- exercises/magicindex/magicindex.js
+++
@@ -1,8 +0,0 @@
-export const magicIndex = (arr, i = 0) => {
-  const j = Math.ceil((arr.length - i) / 2);
-
-  if (arr[i] === i) return i;
-  if (!arr[i]) return -1;
-  if (arr[i] > i) return magicIndex(arr, i - j);
-  if (arr[i] < i) return magicIndex(arr, i + j);
-}
